b3b7321562ac1209cf7dc8ed8dcae5cff436bf7c
OxyDeadbeef <deadbeef.jal@gmail.com>
2015-12-06 21:13:48 -0600

    Initial Commit

---
+++ src/Applet.java
@@ -0,0 +1,19 @@
+package ProcessingProgram.src;
+
+import processing.core.*;
+
+/**
+ * A simple program that shows an orange screen.
+ */
+public class Applet extends PApplet {
+
+	public void settings() {
+		fullScreen();
+		size(640, 480);
+	}
+
+	public void draw() {
+		background(255,127,0);
+	}
+
+}
---
+++ src/MainWrapper.java
@@ -0,0 +1,10 @@
+package ProcessingProgram.src;
+
+import processing.core.*;
+
+public class MainWrapper {
+	
+	public static void main(String args[]) {	
+		PApplet.main(new String[]{ Applet.class.getName() });
+	}
+}
